63a758fd487e92fb44573607f2c3b5f86bf270a7
elberdev <elberdev@gmail.com>
2016-01-11 15:17:30 -0500

    started big O

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -6,25 +6,31 @@ var str = "Hello, playground"
 
 /*
 
-Use the link here to get the questions.  Then code your solutions below.  If it does not require code, just write your answer in comments.
-
-https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
+1) With my new top of the line XJ452 supercomputer, memory access takes 1 picosecond, math operations take 3 picoseconds, and storing data in memory takes 10 picoseconds. My friend wrote a filter that makes a pixel more awesome, and takes 200 picoseconds to run.
 
 
-1)
+    a) How long would my computer take to execute the following code if the input image is 1000px wide by 2000px tall? What if it’s n by m?
 
-2)
 
-3)
+    Pixel **awesomeFilter(Pixel image[][], int width, int height) {
 
-4)
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                [image[i][j] makeMoreAwesome];
+            }
+        }
+    return image;
+    }
 
-5)
+    
 
-6)
 
-7)
+    b) What is the time complexity of this method, expressed in big O notation? Assume the image is square, and both dimensions are ‘n’.
 
+        O(n^2)
+
+
+    c) My friend sends me an improved version of his algorithm, makeEvenMoreAwesome, that takes into account the pixels around the image. He says it’s O(n2) in the amount of pixels in the image. What is the new time complexity of the method?
 
 */
 
