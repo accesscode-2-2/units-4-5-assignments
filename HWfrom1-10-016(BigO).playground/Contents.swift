23ca577502835ba6f132f285c228e77f83062d4e
Mesfin Bekele Mekonnen <mesbekmek@gmail.com>
2016-01-13 15:15:20 -0500

    Finished swift intro, still working on big O

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -11,9 +11,9 @@ Use the link here to get the questions.  Then code your solutions below.  If it 
 https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
 
 
-1)
+1) O(1000 * 2000) = O(2000000) = O(1) , and if n by m O(nm)
 
-2)
+2) O(n^2)
 
 3)
 
