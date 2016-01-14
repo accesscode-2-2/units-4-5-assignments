8ff8ef8a373a6d8cf56e46ff5442e57bb1341660
Henna <henna.ahmed92@gmail.com>
2016-01-14 10:49:11 -0500

    big O hw, not completed

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -11,15 +11,71 @@ Use the link here to get the questions.  Then code your solutions below.  If it 
 https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
 
 
-1)
+1)  Memory access takes 1 picosecond
+    Math operations take 3 picoseconds
+    Storing data in memory takes 10 picoseconds
+    Friends algo takes 200 picoseconds to run. 
+    
+How long would my computer take to execute the following code if the input image is 1000px wide by 2000px tall?
+    (10 + 3 + 3) * 1000
+    (10 + 3 + 3) * 2000
+    2 + 200
+    1
+    = 16000+ 32000 + 202 + 1 = 48,203
+
+    What if it’s n by m?
+    15n + 15m + 203
+
+b) O(n)
+
+c) I'm not sure, O(n^2)
+
 
 2)
-
+    a) O(n^4)
+    b) O(n)
+    c) O(n^2)
+    d) O(n)
 3)
+    a) Tree
 
-4)
+    b) Hash Table
 
-5)
+    c) Tree
+
+
+4)*/
+
+func factorial(n : Int) -> Int {
+    
+    if n <= 1 {
+        return 1
+    }
+    
+    return factorial(n-1)*n
+}
+
+/* time complexity: O(n) */
+
+
+/*
+
+5) */
+func product(number: Int, times: Int) -> Int {
+    
+    var totProduct = 0
+    
+    for _ in 1...times{
+        totProduct = totProduct + number
+    }
+    
+    return totProduct
+}
+product(2, times: 3)
+
+/* complexity is O(n)*/
+
+/*
 
 6)
 
@@ -27,4 +83,5 @@ https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/
 
 
 */
+
 
