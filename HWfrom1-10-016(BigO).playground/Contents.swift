5952b883240bd28e208c6700bb54f8c342c5cf07
charles <ckangkitchen@gmail.com>
2016-01-13 22:55:28 -0500

    sort of finished the big o assignment, have no idea how to do questions 4 and 7...

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -2,7 +2,6 @@
 
 import UIKit
 
-var str = "Hello, playground"
 
 /*
 
@@ -12,19 +11,55 @@ https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/
 
 
 1)
+memory access = 1 pico
+math operations = 3 pico
+storing data = 10 pico
+time to run = 200 pico
+
+a) 241 picoseconds
+b) O(n^2)
+c) O(n^3)
 
 2)
+a) O(n^3), O(n^4) O(n^2)
+b) O(n)
+c) O(n^2 log(n))
+d) O(^2)
+
 
 3)
+a) binary search tree
+b) skip list
+c) maybe you could use two queues? one for storing searches as you type, and another for the searched music
 
-4)
+4) I don't know the answer to this. What exercise 1? The one from Linus? Wording is confusing for me...
 
 5)
-
-6)
-
-7)
-
-
 */
+func multiplyOldSchool() {
+    var num = 3 + 3 + 3 + 3 + 3 = 15
+    //time complexity = O(13)?! I have no idea
+}
+
+/* 
+6) 
+*/
+
+func russianMath() {
+    for (i = 0; i < 104; i++) {
+        13 * 8
+    }
+}
+// complexity should be O(n^2)
+
+// 7) I don't know how to answer this because I don't get question 4. There's only snippet in exercise 1? 
+
+// http://stackoverflow.com/questions/24331566/objective-c-sort-an-array-of-strings-containing-numbers
+// http://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
+// http://stackoverflow.com/questions/11075644/sort-nsmutablearray-with-strings-that-contain-numbers
+
+// I don't even understand what these spreadsheet formulas are. Google spreadsheets?
+
+
+
 
