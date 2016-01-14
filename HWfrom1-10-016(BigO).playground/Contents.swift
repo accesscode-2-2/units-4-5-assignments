d0728b84cd956d5f504861b4e9878c7bf3a8fb4e
jmeggesto <jackie.meggesto@gmail.com>
2016-01-14 15:24:53 -0500

    finishing up other algorithms

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -4,27 +4,146 @@ import UIKit
 
 var str = "Hello, playground"
 
-/*
-
-Use the link here to get the questions.  Then code your solutions below.  If it does not require code, just write your answer in comments.
-
-https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
 
 
-1)
+// Use the link here to get the questions.  Then code your solutions below.  If it does not require code, just write your answer in comments.
 
-2)
-
-3)
-
-4)
-
-5)
-
-6)
-
-7)
+// https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
 
 
-*/
+// 1)
+func AwesomeFilter(array: [Int], width: Int, height: Int) -> Int {
+    
+    var time = 10 //init i, store in memory
+    
+    for (var i = 0; i < width; i++) {
+        
+        time += 10 // init j each iteration of  loop, store in memory
+        time += 3 // math operation i < width
+        time += 1 // access memory of i
+        time += 10 // write to memory
+        time += 3 //math operation
+        
+        for (var j = 0; j < height; j++) {
+            
+            time += 3 // math operation i < width
+            time += 1 // access memory of i
+            time += 10 // write to memory
+            time += 3 //math operation
+            time += 201 // 200 picosecond function, plus 1 psecond of memory access
+        }
+        
+    }
+    
+    
+    return time
+    
+}
+let answer = 436027010 // time in picoseconds
+
+let time_complexity_answer1 = "O(nm)"
+let time_complexity_answer2 = "O(nm(n^2))"
+
+
+
+//2)
+
+//2a) O(n^2)
+
+//2b)
+
+func frobnicate(ys: [Int], m: Int) -> Int {
+    
+    if m == 0 {
+        
+        return 0
+    }
+    return ys[m] + frobnicate(ys, m: m - 1)
+    
+    
+    
+}
+
+//O(log(n))
+
+//2c) O(n^2)
+
+
+
+
+//3)
+
+//4)
+
+
+func factorial(number: Int) -> Int {
+    
+    var product = 1
+    for num in 1...number {
+        
+        product *= num
+        
+    }
+
+    return product
+}
+
+//complexity: O(n)
+
+//5)
+
+func multip(number: Int, byNumber: Int) -> Int {
+    
+    
+    var sum = 0
+    for _ in 1...byNumber  {
+        
+        sum += number
+        
+        
+    }
+    
+    return sum
+    
+    
+}
+
+
+
+//Complexity: Wholly determined by byNumber. Linear time.
+
+//6)
+
+func peasant_mult(var number: Int, var byNumber: Int) -> Int {
+    
+    
+    
+    var sum = 0
+    while byNumber != 0 {
+        
+        
+        if byNumber % 2 != 0 {
+            
+            sum += number
+            
+        }
+        
+        number *= 2
+        byNumber = byNumber / 2
+        
+        
+    }
+    //compexity: O(log(n))
+    return sum
+}
+
+peasant_mult(10, byNumber: 10)
+
+
+//time complexity: 
+
+
+//7)
+
+
 
