41be2fab2672a3ce4b7b703ac75b5f6ed63c6252
ayunav <ayunanyc@gmail.com>
2016-01-14 18:47:45 -0500

    Swfit HW submission

--- HWfrom1-09-16(SwiftIntro).playground/Contents.swift
+++ HWfrom1-09-16(SwiftIntro).playground/Contents.swift
@@ -8,16 +8,94 @@ var str = "Hello, playground"
 
 Use the link here to get the questions.  Then code your solutions below.  If it does not require code, just write your answer in comments.
 
-https://docs.google.com/document/d/1DQ2aCJ_yUZtazzCfb0PaS81bg61V2ZOSxpABh981xSo/edit
+https://docs.google.com/document/d/1DQ2aCJ_yUZtazzCfb0PaS81bg61V2ZOSxpABh981xSo/edit */
+
+// 1) Given an integer N, there is a list of size N-1 that is missing one number from 1 - N(inclusive). Find that number.
+
+/* A series of google search results led to this, and I decided to go with this strategy: https://www.glassdoor.com/Interview/We-have-numbers-from-1-to-52-that-are-put-into-a-51-number-array-what-s-the-best-way-to-find-out-which-number-is-missing-QTN_79443.htm
+What they are looking for is probably how good you are at math and algorithms. My answer would be: 1) Calculate the sum of all numbers stored in the array of size 51. 2) Subtract the sum from (52 * 53)/2 ---- Formula : n (n+1)/2. The result of subtraction is the answer for this question. */
+
+func findMissingNumber(n : Int, numbers : [Int]) -> Int {
+    let totalSum = n * (n + 1) / 2
+    var totalSumOfAllNumbers = 0
+    for i in numbers {
+        totalSumOfAllNumbers += i
+    }
+    return totalSum - totalSumOfAllNumbers
+}
+    findMissingNumber(5, numbers: [1, 4, 3, 2])
+    
+
+// 2) Given a list  of size N containing numbers 1 - N (inclusive). return true if there are duplicates, false if not
+    // this solution is from in-class exercise
+    
+    func hasDuplicates(numbers : [Int]) -> Bool {
+        for var i = 0; i < numbers.count; i++ {
+            for var j = 0; j < numbers.count; j++ {
+                if j != i {
+                    if numbers[i] == numbers[j]{
+                        return false
+                    }
+                }
+            }
+        }
+        
+        return true
+}
+        hasDuplicates([1, 2, 3])
+        hasDuplicates([1, 2, 2, 3, 4])
+    
+// 3) Given two lists, find the smallest value that exists in both lists.
+//L1 = [1,2,5,9]
+//L2 = [9, 20 , 5]
+
+        
+        // this was the second result in google search, and I didn't find anything else: https://gist.github.com/xhenna92/ea27fb0496a3ae0ee334
+        
+        func findMin(arr1 : [Int], arr2 : [Int]) -> Int {
+            
+            // return (arr1.minElement()! > arr2.minElement()! ? arr2.minElement()! : arr1.minElement()!)
+            
+            var minArr1 = arr1.first
+            var minArr2 = arr2.first
+            
+            for var i = 0; i < arr1.count; i++ {
+                
+                if arr1[i] < minArr1 {
+                    minArr1 = arr1[i]
+                }
+                
+            }
+            
+            for var j = 0; j < arr2.count; j++ {
+                
+                if arr2[j] < minArr2 {
+                    minArr2 = arr2[j]
+                }
+                
+            }
+            
+            return (minArr1! > minArr2! ? minArr2 : minArr1)!
+}
+
+findMin([1, 2, 2, 3, 4], arr2: [-1, 2, 2, 3, 4])
+
+findMin([1,2,5,9],arr2: [9, 20 , 5]) // the function doesn't work
 
 
-1)
-
-2)
-
-3)
-
-4)
-
-
-*/
+// 4) Check to see if an integer is a palindrome don’t use casting
+        // http://beginnersbook.com/2015/02/c-program-to-check-if-a-number-is-palindrome-or-not/ using recursion
+        // divide each digit and rebuild number again then compare with original:
+        func isPalindrome(var num: Int) -> Bool {
+            let originalNum = num
+            var finalNum = 0
+            
+            while(num > 0) {
+                finalNum *= 10
+                finalNum += num % 10
+                num  /= 10
+            }
+            return finalNum == originalNum // return true if is palindrome
+        }
+isPalindrome(134543) // test it
+isPalindrome(232)
--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -10,6 +10,88 @@ Use the link here to get the questions.  Then code your solutions below.  If it 
 
 https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
 
+1) With my new top of the line XJ452 supercomputer, memory access takes 1 picosecond, math operations take 3 picoseconds, and storing data in memory takes 10 picoseconds. My friend wrote a filter that makes a pixel more awesome, and takes 200 picoseconds to run.
+How long would my computer take to execute the following code if the input image is 1000px wide by 2000px tall? What if it’s n by m?
+
+
+Pixel **awesomeFilter(Pixel image[][], int width, int height) {
+for (int i = 0; i < width; i++) {
+for (int j = 0; j < height; j++) {
+[image[i][j] makeMoreAwesome];
+}
+}
+return image;
+}
+
+
+What is the time complexity of this method, expressed in big O notation? Assume the image is square, and both dimensions are ‘n’.
+My friend sends me an improved version of his algorithm, makeEvenMoreAwesome, that takes into account the pixels around the image. He says it’s O(n2) in the amount of pixels in the image. What is the new time complexity of the method?
+
+
+If foo(xs) is a function with time complexity n (where n is the size of the input array), and bar(xs) is a function with time complexity n2, what is the time complexity of each of the following snippets of code or algorithms?
+
+
+for (int i = 0; i < n; i++) {
+for (int j = 0; j < n; j++) {
+foo(xs);
+}
+}
+for (int i = 0; i < n; i++) {
+for (int j = 0; j < n; j++) {
+bar(xs);
+}
+}
+for (int i = 0; i < n; i++) {
+for (int j = 0; j < n; j++) {
+// do cool stuff
+}
+}
+
+
+int frobnicate(ys, m) {
+if (m == 0) {
+return 0;
+}
+return ys[m] + frobnicate(ys, m - 1);
+}
+frobnicate(xs, n);
+
+Tip: Write down a table with n from 0 to 5 and trace through to find out how many times frobnicate is called with each value of n.
+
+
+An algorithm that takes as its input a list of friends of length n, filters out duplicates using a method similar to our hasDuplicates method, sorts the list using merge sort (see bigocheatsheet.com), then prints each item to the screen.
+
+
+An algorithm that searches the now-sorted list of friends for a specific friend (not including the time it takes to sort).
+
+
+Look at the complexities for some common data structures at bigocheatsheet.com. Pick a good data structure for each of the following scenarios (there are sometimes multiple answers):
+
+
+You get a large dataset of points of interest from an API when your app first runs. You build it once at the beginning, and then have to search it many times while the user pans around a map.
+
+
+You get a small dataset of points of interest from an API every time the user pans the map. You construct the data set many times and only render it once, then you discard it and do another API search.
+
+Tip: Constructing a dataset of size n means you have to call the data structure’s insert method n times. So if the data structure has an insert method that takes O(n2), the time to build it all from scratch is O(n3).
+
+
+You used a linked list for your music app’s playlist feature, but now when people search their playlist, there’s a noticeable lag before loading results. Your competitor’s app is buttery smooth when searching, even showing results as you type. What data structure would allow you to more quickly search without compromising too much on the speed of inserting and deleting tracks, even in the worst case?
+
+
+Write an algorithm using one of the methods from exercise 1 (your choice) to calculate the factorial of a number n. What is the time complexity of your method in terms of the input value?
+
+
+Write an Objective C or Swift function to multiply two numbers without using the * operator. Use the grade school method of multiplying by doing repeated addition. For instance, 5 * 8 = 5 + 5 + 5 + 5 + 5 + 5 + 5 + 5 = 40. Find the big O of your function in terms of n and m (the two operands).
+
+
+Look up Russian Peasant Multiplication. It’s a faster way to multiply numbers, especially on a binary computer (like yours!). Implement a new multiplication function using this technique and find the big O of your method. If you have trouble with implementing this, write a flow chart and find the big O based on that. (But it’s more satisfying to implement it and run it)
+
+Tip: Run through the method by hand a few times to see how it works and verify to yourself that it does. It’s a non-intuitive algorithm. This will hopefully also make the time complexity more clear.
+
+
+Using the technique from exercise 4, profile the built in sorting method in objective C (use an NSMutableArray and google how to sort an array of numbers in objective C). Graph the result. Use spreadsheet formulas to add graph lines for n, n2, and n*log(n). (You’ll have to modify the factors to make them fit in the graph window and to be close to the graph of method execution time). Show that the sort method best fits n * log(n).
+
 
 1)
 
