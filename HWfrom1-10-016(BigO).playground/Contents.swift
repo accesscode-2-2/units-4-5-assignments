b4965c8cfdd8f4f7fb76eeeed13faac24d71ef69
Xiulan Shi <xiulanshi2013@gmail.com>
2016-01-14 14:52:53 -0500

    Updated

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -17,15 +17,15 @@ a) How long would my computer take to execute the following code if the input im
 
 
 Pixel **awesomeFilter(Pixel image[][], int width, int height) {
-    for (int i = 0; i < width; i++) {   //1000*5
-        for (int j = 0; j < height; j++) { //1000*2000*5
+    for (int i = 0; i < width; i++) {   //1000*3
+        for (int j = 0; j < height; j++) { //1000*2000*3
             [image[i][j] makeMoreAwesome];  // 1000*2000*2+1000*2000*200+1000*2000*10
         }
     }
     return image;
 }
 
-434,005,000 picoseconds. O(n*m)  ( 217nm+3n) if it's n by m.
+430,003,000 picoseconds. O(n*m)  ( 215nm+3n) if it's n by m.
 
 b) What is the time complexity of this method, expressed in big O notation? Assume the image is square, and both dimensions are ‘n’.
 
@@ -98,12 +98,12 @@ copy leftPartHeadValue
 else: copy rightPartHeadValue
 copy elements back to original array
 
-
+O(n(logn))
 
 
 
 d) An algorithm that searches the now-sorted list of friends for a specific friend (not including the time it takes to sort).
-
+O(n)
 
 */
 
@@ -111,21 +111,76 @@ d) An algorithm that searches the now-sorted list of friends for a specific frie
 
 
 a) You get a large dataset of points of interest from an API when your app first runs. You build it once at the beginning, and then have to search it many times while the user pans around a map.
+Tree----A quad tree is a data structure comprising nodes which store a bucket of points and a bounding box. Any point which is contained within the node’s bounding box is added to its bucket. Once the bucket gets filled up, the node splits itself into four nodes, each with a bounding box corresponding to a quadrant of its parents bounding box. All points which would have gone into the parent’s bucket now go into one of its children’s buckets.
 
+https://robots.thoughtbot.com/how-to-handle-large-amounts-of-data-on-maps
 
 b) You get a small dataset of points of interest from an API every time the user pans the map. You construct the data set many times and only render it once, then you discard it and do another API search.
+Hash Table
 
 Tip: Constructing a dataset of size n means you have to call the data structure’s insert method n times. So if the data structure has an insert method that takes O(n2), the time to build it all from scratch is O(n3).
 
 
 c) You used a linked list for your music app’s playlist feature, but now when people search their playlist, there’s a noticeable lag before loading results. Your competitor’s app is buttery smooth when searching, even showing results as you type. What data structure would allow you to more quickly search without compromising too much on the speed of inserting and deleting tracks, even in the worst case?
-
+Binary Tree
 */
 
 /*4)Write an algorithm using one of the methods from exercise 1 (your choice) to calculate the factorial of a number n. What is the time complexity of your method in terms of the input value?
+
+func factorial(N: Int) -> Int {
+if N == 1 {
+return 1
+} else {
+return N * factorial(N - 1)
+}
+}
+
+o(n!)
+
 */
 
 /*5)Write an Objective C or Swift function to multiply two numbers without using the * operator. Use the grade school method of multiplying by doing repeated addition. For instance, 5 * 8 = 5 + 5 + 5 + 5 + 5 + 5 + 5 + 5 = 40. Find the big O of your function in terms of n and m (the two operands).
+
+http://www.geeksforgeeks.org/multiply-two-numbers-without-using-multiply-division-bitwise-operators-and-no-loops/
+/* function to multiply two numbers x and y*/
+int multiply(int x, int y)
+{
+/* 0  multiplied with anything gives 0 */
+if(y == 0)
+return 0;
+
+/* Add x one by one */
+if(y > 0 )
+return (x + multiply(x, y-1));
+
+/* the case where y is negative */
+if(y < 0 )
+return -multiply(x, -y);
+}
+
+int main()
+{
+printf("\n %d", multiply(5, -11));
+getchar();
+return 0;
+}
+
+Read more:  http://www.noexit4u.com/2013/03/c-program-to-multiply-two-numbers.html
+
+    {
+        int num1,num2,i,prod=0;
+        clrscr();
+        printf("Enter 1st number: ");
+        scanf("%d",&num1);
+        printf("Enter 2nd number: ");
+        scanf("%d",&num2);
+        for(i=1;i<=num2;i++)
+        {
+            prod+=num1;
+        }
+        printf("\n\n\nProduct of %d and %d is %d",num1,num2,prod);
+        getch();
+}
 */
 
 /*6)Look up Russian Peasant Multiplication. It’s a faster way to multiply numbers, especially on a binary computer (like yours!). Implement a new multiplication function using this technique and find the big O of your method. If you have trouble with implementing this, write a flow chart and find the big O based on that. (But it’s more satisfying to implement it and run it)
