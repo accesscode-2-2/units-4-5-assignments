c689f7f1c5a1af7ea121da23c536ef8d7919a4eb
Umar <umahmud@outlook.com>
2016-01-13 23:55:12 -0500

    commit

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -11,13 +11,23 @@ Use the link here to get the questions.  Then code your solutions below.  If it 
 https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
 
 
-1)
+1) a) if input is 1000px by 2000px -> 1000 * 2000 = 2 000 000 + 200 = 2 000 200
 
-2)
+      if input is n, m ->  nm + 200
 
-3)
+   b) O(n^2)
+   c) 0(n^4)
 
-4)
+2) a)n^2 + n^4 + n^2 -> O(n^4)
+   b)O(n)
+   c)O(log(n))
+   d)O(log(n))
+
+3)a)hash-table
+  b)stack
+  c) b-tree
+
+4) 
 
 5)
 
