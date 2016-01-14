9fbd8cda73555c327ff2f1e3681cdd64f1441418
Mesfin Bekele Mekonnen <mesbekmek@gmail.com>
2016-01-14 00:41:51 -0500

    Q1 and Q2 for Big O practice

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -11,10 +11,18 @@ Use the link here to get the questions.  Then code your solutions below.  If it 
 https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
 
 
-1) O(1000 * 2000) = O(2000000) = O(1) , and if n by m O(nm)
+1)a. (1 + 3 + 10) * 1000 + (1 + 3 + 10) * 2000 + 200 * (1000 * 2000)
+     14,000ps + 28,000ps + 400000000ps = 400042000ps
 
-2) O(n^2)
+  b. O(n^2)
+  c. O(n^4)
 
+
+2)a. O(n^3) + O(n^4) + O(n^2) = O(n^4)
+  b. frobnicate(xs, n) = xs[n] + xs[n-1] + xs[n-2] + ... + xs[1] + 0, so for n we make n+1 calls to frobnicate so it's 0(n)
+
+  c. O(n) + O(nlog n) + O(n) = O(nlog n)
+  d. O(log n)
 3)
 
 4)
