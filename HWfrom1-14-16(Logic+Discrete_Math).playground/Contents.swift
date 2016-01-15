03ca6f52e25b609cd0a08ea167ddde27b6c5cb5b
Henna <henna.ahmed92@gmail.com>
2016-01-15 17:28:17 -0500

    logic and discrete clean up

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -8,14 +8,7 @@ var str = "Hello, playground"
 /*
 
 Question 1: https://www.hackerrank.com/challenges/minimum-draws
-Jim is off to a party and is searching for a matching pair of socks. His drawer is filled with socks, each pair of a different color. In its worst case scenario, how many socks (x) should Jim remove from his drawer until he finds a matching pair?
 
-Input Format
-The first line contains the number of test cases T.
-Next T lines contains an integer N which indicates the total pairs of socks present in the drawer.
-
-Output Format
-Print the number of Draws (x) Jim makes in the worst case scenario.
 
 Copy and paste your code:
 
@@ -26,25 +19,11 @@ let nextLine = Int(readLine(stripNewline: true)!)!
 print(nextLine+1)
 }
 
-*/
-
-
-
-
-/*
 Jim would have to draw x + 1 socks in the worst case. where x is the number of pairs of socks
 
 What is the big O runtime of your code?: O(n)
 
 Question 2: https://www.hackerrank.com/challenges/handshake
-At the annual meeting of Board of Directors of Acme Inc, every one starts shaking hands with everyone else in the room. Given the fact that any two persons shake hand exactly once, Can you tell the total count of handshakes?
-
-Input Format
-The first line contains the number of test cases T, T lines follow.
-Each line then contains an integer N, the total number of Board of Directors of Acme.
-
-Output Format
-Print the number of handshakes for each test-case in a new line.
 
 Copy and paste your code:
     let firstLine = Int(readLine(stripNewline: true)!)!
@@ -60,6 +39,7 @@ What is the big O runtime of your code?: O(log(n))
 Question 3: https://www.hackerrank.com/challenges/connecting-towns
 
 Copy and paste your code:
+
 let firstLine = Int(readLine(stripNewline: true)!)!
 for _ in 0...firstLine{
     let town = Int(readLine(stripNewline: true)!)!
