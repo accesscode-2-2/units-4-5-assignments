5d102f11068a4cd05f2a31bb020e1458bce91fae
Jason Wang <qwang216@gmail.com>
2016-01-14 19:04:45 -0500

    hw

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -14,11 +14,21 @@ https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/
 1)
     a) ANS = 160 ps
     b) O(n^2)
-    c) O(n^4)
+    c) O(n^2)
 
-2) a) n^4 + n^3 + n^2
-    b) n^m
-    
+2) a) n^4 , n^3 , n^2
+    b) O(n!)
+    c) 
+    func isDuplication -> Bool {
+    let friendsArray = ["mike","joe","hello"]
+    let unique = Array(Set(friendsArray))
+    if  (friendsAray == unique) {
+    return false
+    } else {
+    return true
+    }
+    }
+
 
 3)
 
