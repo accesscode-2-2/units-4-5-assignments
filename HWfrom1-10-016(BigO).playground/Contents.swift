2fc2ae3d540dd7c8369ef68f2a912cf3449bf1e7
elberdev <elberdev@gmail.com>
2016-01-14 19:14:03 -0500

    correcting

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -14,12 +14,19 @@ var str = "Hello, playground"
 
     Pixel **awesomeFilter(Pixel image[][], int width, int height) {
 
+
         for (int i = 0; i < width; i++) {
+
+
             for (int j = 0; j < height; j++) {
+
+
                 [image[i][j] makeMoreAwesome];
             }
         }
-    return image;
+
+
+        return image;
     }
 
 
@@ -42,9 +49,7 @@ var str = "Hello, playground"
 
     c) My friend sends me an improved version of his algorithm, makeEvenMoreAwesome, that takes into account the pixels around the image. He says it’s O(n2) in the amount of pixels in the image. What is the new time complexity of the method?
 
-        ANSWER: if we calculate using not only the pixels in the image but also the pixels around the image, the new
-        dimension of the array is (n + 2) by (n + 2) which reduces to n^2 + 4n + 4. Since n^2 is still the most relevant
-        determining factor, the complexity of the method is still O(n^2).
+        ANSWER: O(n^4)
 
 
 ******************************************************************************************************************************
@@ -93,6 +98,44 @@ var str = "Hello, playground"
     d) An algorithm that searches the now-sorted list of friends for a specific friend (not including the time it takes to sort).
 
         ANSWER: Using binary search, O(log(n))
+
+
+*****************************************************************************************************************************
+
+    3) Look at the complexities for some common data structures at bigocheatsheet.com. Pick a good data structure for each of the following scenarios (there are sometimes multiple answers):
+
+
+            a) You get a large dataset of points of interest from an API when your app first runs. You build it once at the beginning, and then have to search it many times while the user pans around a map.
+
+
+            b) You get a small dataset of points of interest from an API every time the user pans the map. You construct the data set many times and only render it once, then you discard it and do another API search.
+
+                Tip: Constructing a dataset of size n means you have to call the data structure’s insert method n times. So if the data structure has an insert method that takes O(n2), the time to build it all from scratch is O(n3).
+
+
+            c) You used a linked list for your music app’s playlist feature, but now when people search their playlist, there’s a noticeable lag before loading results. Your competitor’s app is buttery smooth when searching, even showing results as you type. What data structure would allow you to more quickly search without compromising too much on the speed of inserting and deleting tracks, even in the worst case?
+
+
+******************************************************************************************************************************
+
+    4) Write an algorithm using one of the methods from exercise 1 (your choice) to calculate the factorial of a number n. What is the time complexity of your method in terms of the input value?
+
+
+******************************************************************************************************************************
+
+    5) Write an Objective C or Swift function to multiply two numbers without using the * operator. Use the grade school method of multiplying by doing repeated addition. For instance, 5 * 8 = 5 + 5 + 5 + 5 + 5 + 5 + 5 + 5 = 40. Find the big O of your function in terms of n and m (the two operands).
+
+
+******************************************************************************************************************************
+
+    6) Look up Russian Peasant Multiplication. It’s a faster way to multiply numbers, especially on a binary computer (like yours!). Implement a new multiplication function using this technique and find the big O of your method. If you have trouble with implementing this, write a flow chart and find the big O based on that. (But it’s more satisfying to implement it and run it)
+
+        Tip: Run through the method by hand a few times to see how it works and verify to yourself that it does. It’s a non-intuitive algorithm. This will hopefully also make the time complexity more clear.
+
+
+*****************************************************************************************************************************
+
+    7) Using the technique from exercise 4, profile the built in sorting method in objective C (use an NSMutableArray and google how to sort an array of numbers in objective C). Graph the result. Use spreadsheet formulas to add graph lines for n, n2, and n*log(n). (You’ll have to modify the factors to make them fit in the graph window and to be close to the graph of method execution time). Show that the sort method best fits n * log(n).
 
 */
 
