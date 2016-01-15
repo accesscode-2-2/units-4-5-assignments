d8da236ebbf635ee74b60d002af410c3093f215d
ayunav <ayunanyc@gmail.com>
2016-01-15 16:33:41 -0500

    pulled hw from github

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -8,10 +8,10 @@ var str = "Hello, playground"
 
 Use the link here to get the questions.  Then code your solutions below.  If it does not require code, just write your answer in comments.
 
-https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
+https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth */
 
-1) With my new top of the line XJ452 supercomputer, memory access takes 1 picosecond, math operations take 3 picoseconds, and storing data in memory takes 10 picoseconds. My friend wrote a filter that makes a pixel more awesome, and takes 200 picoseconds to run.
-How long would my computer take to execute the following code if the input image is 1000px wide by 2000px tall? What if it’s n by m?
+/*1) With my new top of the line XJ452 supercomputer, memory access takes 1 picosecond, math operations take 3 picoseconds, and storing data in memory takes 10 picoseconds. My friend wrote a filter that makes a pixel more awesome, and takes 200 picoseconds to run.
+How long would my computer take to execute the following code if the input image is 1000px wide by 2000px tall? What if it’s n by m? */
 
 
 Pixel **awesomeFilter(Pixel image[][], int width, int height) {
@@ -24,7 +24,7 @@ return image;
 }
 
 
-What is the time complexity of this method, expressed in big O notation? Assume the image is square, and both dimensions are ‘n’.
+/* What is the time complexity of this method, expressed in big O notation? Assume the image is square, and both dimensions are ‘n’.
 My friend sends me an improved version of his algorithm, makeEvenMoreAwesome, that takes into account the pixels around the image. He says it’s O(n2) in the amount of pixels in the image. What is the new time complexity of the method?
 
 
