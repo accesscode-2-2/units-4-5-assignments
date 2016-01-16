ddbee84b067928a21ae0e993fd6d579729b2bed0
JovannyEspinal <espinaljovanny@icloud.com>
2016-01-15 19:18:10 -0500

    Only 2 out of 3.

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -1,29 +1,63 @@
 //: Playground - noun: a place where people can play
-
+import Foundation
 import UIKit
 
 var str = "Hello, playground"
 
 
-/*
 
-Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
-Copy and paste your code:
+// Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
-What is the big O runtime of your code?:
+func maximumDraws(testCases: Int) {
+    var numberOfPairs = 0
+    var draws = 0
+    
+    for _ in 1...testCases {
+        numberOfPairs = Int(readLine(stripNewline: true)!)!
+        draws = numberOfPairs + 1
+        
+        print(draws)
+    }
+}
 
-Question 2: https://www.hackerrank.com/challenges/handshake
+let testCases = Int(readLine(stripNewline: true)!)!
+maximumDraws(testCases)
 
-Copy and paste your code:
 
-What is the big O runtime of your code?:
 
-Question 3: https://www.hackerrank.com/challenges/connecting-towns
+// What is the big O runtime of your code?: O(n)
 
-Copy and paste your code:
+// Question 2: https://www.hackerrank.com/challenges/handshake
 
-What is the big O runtime of your code?:
 
-*/
+func numberOfHandShakes(testCase: Int){
+    var numberOfBoardDirectors = 0
+    var handshakes = 0
+    
+    for _ in 1...testCases {
+        numberOfBoardDirectors = Int(readLine(stripNewline: true)!)!
+        handshakes = numberOfBoardDirectors * (numberOfBoardDirectors - 1)/2
+        
+        print(handshakes)
+    }
+}
+
+let testCases = Int(readLine(stripNewline: true)!)!
+numberOfHandShakes(testCases)
+
+
+
+// What is the big O runtime of your code?: O(n)
+
+// Question 3: https://www.hackerrank.com/challenges/connecting-towns
+
+
+
+// What is the big O runtime of your code?:
+
+
+
+
+
 
