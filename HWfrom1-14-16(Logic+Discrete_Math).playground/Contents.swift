bf69c7b2219c9ef89bb58a7b10287736d5d67650
Xiulan Shi <xiulanshi2013@gmail.com>
2016-01-15 23:17:21 -0500

    Finished 1 and 2

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -9,15 +9,31 @@ var str = "Hello, playground"
 
 Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
-Copy and paste your code:
+Copy and paste your code: 
 
-What is the big O runtime of your code?:
+let firstLine = Int(readLine(stripNewline: true)!)!
+for _ in 1...firstLine{
+let totalPairOfSocks = Int(readLine(stripNewline: true)!)!
+print(totalPairOfSocks+1)
+}
+
+//In its worst case scenario, n+1 socks (x) should Jim remove from his drawer until he finds a matching pair if there are n pairs socks in total.
+
+What is the big O runtime of your code?: O(n)
 
 Question 2: https://www.hackerrank.com/challenges/handshake
 
 Copy and paste your code:
 
-What is the big O runtime of your code?:
+let firstLine = Int(readLine(stripNewline: true)!)!
+for _ in 1...firstLine{
+let numberOfPeople = Int(readLine(stripNewline: true)!)!
+print(numberOfPeople*(numberOfPeople - 1)/2)
+}
+
+// n*(n-1)/2 which is simplified from C(n, 2) 
+
+What is the big O runtime of your code?: O(n^2)
 
 Question 3: https://www.hackerrank.com/challenges/connecting-towns
 
