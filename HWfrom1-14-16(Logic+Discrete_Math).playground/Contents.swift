038a42abe038c225c86c02b829fc13b5ec5a5df8
charles <ckangkitchen@gmail.com>
2016-01-15 19:52:31 -0500

    jan16 homework submission

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -9,21 +9,58 @@ var str = "Hello, playground"
 
 Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
+// Could you write a for loop that keeps on looping until it finds the matching pair? Use n + 1 for each iteration until you find the right pair.
+
+var numberOfDraws = NSArray()
+
+// n + 1 until matching pair is found
+// I do not know how to solve this with code...I can sort of understand and process it just by reading the question,
+// but having to put it into code, especially swift, is really hard for me
+
+/*
 Copy and paste your code:
 
 What is the big O runtime of your code?:
+
+O(n) since you only need 1 for loop and you cancel the other variables out which leaves O(n)
 
 Question 2: https://www.hackerrank.com/challenges/handshake
 
+use the non-repeating formula
+
+if there are 5 people shaking hands but only once and never repeating
+
+1 x 2
+1 x 3
+1 x 4
+1 x 5 
+= 4 handshakes
+
+n! / (r! * (n-r)!)
+
+
 Copy and paste your code:
 
 What is the big O runtime of your code?:
+O((n^2)
 
 Question 3: https://www.hackerrank.com/challenges/connecting-towns
 
 Copy and paste your code:
 
-What is the big O runtime of your code?:
+I genuinely do not know the answers to these questions. Googling doesn't help because I can't find a decent solution and step by step process, and the fact that this is considered easy stresses me out like crazy... 
+
+I'm assuming Gandalf will want to only go through each town once, so use the non-repeating combination formula: n! / (r!*(n-r)!)
+
+What is the big O runtime of your code? 
+O(n^2)
 
 */
 
+*/
+
+
+class Solution {
+    var int = 7
+    
+}
