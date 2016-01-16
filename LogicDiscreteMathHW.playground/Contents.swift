6512cbf56de09874fff50c0aafc14c8579c8db35
Felicia Weathers <fel.weathers@gmail.com>
2016-01-16 00:11:42 -0500

    LogicDiscreteMathHW

---
+++ LogicDiscreteMathHW.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+1.
+let n = number of socks
+max number of socks drawn for a pair = (n/2) + 1
+O(n) bc only need one for loop
+
+2.
+
+C(n,2)
+
+n! / (2! * (n-2)!)
+
+3.
+
+*/
+
+
+
+
+    
