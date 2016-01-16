ac597fff342a71cfc5c5f0eb2c7f95053448df41
cdolm92 <cdolm92@gmail.com>
2016-01-16 01:23:03 -0500

    a problem done

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -27,3 +27,44 @@ What is the big O runtime of your code?:
 
 */
 
+//1. The number of draws he makes in the worst case scenario is 3. I don't know how to express that in code.
+
+//2
+
+
+func factorial(var value: Int, var result: Int = 1) -> Int {
+    
+    if (value == 0) {
+        return result
+    }
+    
+    result *= value
+    value--
+    
+    return factorial(value, result: result)
+    }
+    
+func handshakes(attendees: Int) -> Int {
+    
+  let num = factorial(attendees)
+        
+  let den = (factorial(2) * factorial(attendees - 2))
+    
+  let total = num / den
+    
+    return total
+    
+}
+
+
+handshakes(4)
+handshakes(2)
+
+
+
+
+
+
+
+
+
