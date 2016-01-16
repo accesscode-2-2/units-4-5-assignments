96248d0e80fecfdae3516fc274ecd57ad9ec6eb6
jamaal51 <jsedayao@gmail.com>
2016-01-16 09:14:19 -0500

    hw discrete

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -11,11 +11,28 @@ Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
 Copy and paste your code:
 
+var socksPulledOut = []
+
+for sock in socksPulledOut {
+    if sock == sock.last {
+        return true
+    } else {
+        return false
+}
+
 What is the big O runtime of your code?:
 
 Question 2: https://www.hackerrank.com/challenges/handshake
 
 Copy and paste your code:
+
+var N = []
+var handshakes = 0
+
+for boardmember in 0..boardmembers {
+
+}
+
 
 What is the big O runtime of your code?:
 
