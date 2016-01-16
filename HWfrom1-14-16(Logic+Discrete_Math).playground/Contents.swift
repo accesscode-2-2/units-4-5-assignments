60769e19553267113be2159e6c3da083a4b990df
Varindra <varindrahart@gmail.com>
2016-01-16 01:53:22 -0500

    complete

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -10,20 +10,51 @@ var str = "Hello, playground"
 Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
 Copy and paste your code:
+*/
+func maxDraws(pairs: Int) -> Int? {
 
-What is the big O runtime of your code?:
+    guard pairs>0 else{
+        return nil
+    }
+
+    if pairs == 1{
+        return 2
+    }
+    else{
+        return pairs/2 + 1
+    }
+
+}
+/*
+What is the big O runtime of your code?: O(1)
 
 Question 2: https://www.hackerrank.com/challenges/handshake
 
 Copy and paste your code:
-
-What is the big O runtime of your code?:
+*/
+func handshakes(people: Int)-> Int{
+    return (people-1)*(people)/2
+}
+/*
+What is the big O runtime of your code?: O(1)
 
 Question 3: https://www.hackerrank.com/challenges/connecting-towns
 
 Copy and paste your code:
+*/
+func totalPaths(seq : [Int]) -> Int {
 
-What is the big O runtime of your code?:
+    var paths = seq.count>0 ? 1 : 0
+
+    for n in seq {
+        paths *= n
+    }
+
+    return paths
+}
+
+/*
+What is the big O runtime of your code?: O(n)
 
 */
 
