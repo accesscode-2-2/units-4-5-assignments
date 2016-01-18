d5a9c7c481837c154b16660d63f5950c64787342
elberdev <elberdev@gmail.com>
2016-01-18 11:30:11 -0500

    upstream merge and hackerrank submissions

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -151,7 +151,7 @@ var str = "Hello, playground"
 
     7) Using the technique from exercise 4, profile the built in sorting method in objective C (use an NSMutableArray and google how to sort an array of numbers in objective C). Graph the result. Use spreadsheet formulas to add graph lines for n, n2, and n*log(n). (You’ll have to modify the factors to make them fit in the graph window and to be close to the graph of method execution time). Show that the sort method best fits n * log(n).
 
-        ANSWER:
+        ANSWER: ??
 
 */
 
--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -4,26 +4,77 @@ import UIKit
 
 var str = "Hello, playground"
 
-
 /*
-
 Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
-Copy and paste your code:
+Copy and paste your code: */
 
-What is the big O runtime of your code?:
+let testCases = Int(readLine(stripNewline: true)!)!
+
+for _ in 0..<testCases {
+    
+    let totalPairOfSocks = Int(readLine(stripNewline: true)!)!
+    
+    print(totalPairOfSocks + 1)
+}
+
+/*
+What is the big O runtime of your code?: O(n)
 
 Question 2: https://www.hackerrank.com/challenges/handshake
 
-Copy and paste your code:
+Copy and paste your code:*/
 
-What is the big O runtime of your code?:
+let testCases2 = Int(readLine(stripNewline: true)!)!
+
+for _ in 0..<testCases2 {
+    
+    let numberOfPeople = Int(readLine(stripNewline: true)!)!
+    
+    print(numberOfPeople * (numberOfPeople - 1) / 2)
+}
+
+/*
+What is the big O runtime of your code?: O(n)
 
 Question 3: https://www.hackerrank.com/challenges/connecting-towns
 
-Copy and paste your code:
+Copy and paste your code: */
 
-What is the big O runtime of your code?:
+// the first line is the number of test cases
+let numOfTestCases = Int(readLine(stripNewline: true)!)!
 
+// after the first line we have alternating lines with a) the number of towns and b) the numbers of
+// different paths between each town. We will parse those now...
+
+for _ in 0..<numOfTestCases {
+    
+    // skip the line with the number of towns. We don't need it for anything
+    readLine(stripNewline: true)!
+    
+    // now we are at the line we need to calculate the answer.
+    // These are the actual values we care about and need to multiply together.
+    // For now it is a string with a bunch of whitespace. EX: "2 2 3"
+    let rawPathNumbers = readLine(stripNewline: true)!
+    
+    // remove the whitespace. EX: "223"
+    let rawPathNumsNoSpace = rawPathNumbers.characters.split{ $0 == " " }.map(String.init)
+    
+    // convert the string into an Int array. EX: [2, 2, 3]
+    let intArray = rawPathNumsNoSpace.map{ Int(String($0)) ?? 0 }
+    
+    // multiply the Ints together. EX: 2 * 2 * 3 = 12
+    var total = 1
+    for num in intArray {
+        
+        total *= num     // multiply and keep a running tally
+        total %= 1234567 // avoid overflow on large test cases
+    }
+    
+    print(total)
+}
+
+/*
+What is the big O runtime of your code?: O(n)
 */
 
