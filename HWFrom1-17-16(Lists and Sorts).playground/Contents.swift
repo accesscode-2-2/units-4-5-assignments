4eef9cd663275a5ece726560cf4c08d983c5903e
Justine Gartner <GartnerJustine@gmail.com>
2016-01-18 19:41:43 -0500

    Began working on HWFrom1-17-16

--- HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
+++ HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
@@ -9,18 +9,51 @@ var str = "Hello, playground"
 
 Work on your solutions here.
 
+
+
+
 Link: https://docs.google.com/document/d/1XioaEqk6VqUPA-ccQhkqP3eAoDthxYyOM9vSPB7fDkg/edit#heading=h.uopysoy45zmw
 
-1) 
+1) Given a partially filled in Sudoku board and a set of coordinates in that board pointing to an empty square, write a function that returns a list containing all numbers that the empty square could be
+
+    Input: sudokuBoard:[[Int?]]. (Each location on the board will be either be an Int from 1-9 or nil(empty cell))
+    row: Int
+    col: Int
+*/
+
+func getValidNumbers(sudokuBoard:[[Int?]], row:Int, col:Int) -> [Int] {
+    
+    
+    return []
+}
 
 
 
-2) 
+/*
+
+2)rotate a matrix by ninety degrees
+Input: matrix:[[Int]]
+Output: matrix: [[Int]]
+
+Sample Input: [ [1][2][3][4],
+[5][6][7][8],
+[9][0][1][2],
+[3][4][5][6] ]
+Sample Output: [ [3][9][5][1],
+[4][0][6][2],
+[5][1][7][3],
+[6][2][8][4] ]
+*/
 
 
+/*
 
-3)
+3)Design an optimal algorithm for sorting four elements A, B, C, and D. 
+    
+    By optimal, I mean one that sorts using the minimum number of comparisons.
+    Hint: you may want to start by putting the first two items in order 
+    and the last two items in order... that takes two comparisons. 
 
-
-
+    How many more comparisons do you need to find the minimum element? The maximum? 
+    Once you’ve found the min and max, what if any additional comparisons are needed?
 */
