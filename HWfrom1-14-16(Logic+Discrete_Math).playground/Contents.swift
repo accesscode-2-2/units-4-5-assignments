d9c9d91834d675934b1d9d1d85c121969993f829
Fatima <fzf.villanueva@gmail.com>
2016-01-21 11:29:54 -0500

    good

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -46,7 +46,7 @@ for i in 0..<test1{                        // iterates through the number of tes
     print(maxDraws)                          // returns the maximum draw
 }
 
-// The Big-O is O(n)
+// The Big-O is O(1)
 
 // #2
 let test2 : Int = userInput()          // asks for how many tests do you want done
@@ -55,7 +55,7 @@ for i in 0..<test2{                    // iterates through the number of tests i
     handshake = (handshake * (handshake - 1)) / 2
     print(handshake)
 }
-// The Big-O is O(n)
+// The Big-O is O(1)
 
 
 // #3
@@ -67,14 +67,33 @@ while (T > 1){
     for i in 0..<N-1{
         var temp = userInput()
         total = (temp * total) % 1234567
-        
     }
     --T
 }
+
+// Enter your code here
+
+let T = Int(readLine()!)!
+
+for i in 0..<T {
+    let n = Int(readLine()!)!
+    let arr = readLine()!.characters.split(" ").map(String.init).map({Int($0)!})
+    var paths = 1
+    for i in arr {
+        paths *= i
+        paths = paths%1234567
+    }
+    print(paths)
+}
+
+var testCase = userInput() // asks the user for how many test cases
+
+while (testCase > 1){
+    let arr
 // The Big-O is O(n^2)
 // Fundamental of counting principle
 // Multiply them altogether
 // Multiply the combinations
 
-
+}
 
