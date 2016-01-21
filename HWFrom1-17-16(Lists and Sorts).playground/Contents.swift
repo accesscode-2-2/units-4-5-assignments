676d8eb117221cf6f16824180ba7a1b10383a64a
Umar <umahmud@outlook.com>
2016-01-21 18:59:29 -0500

    made some progress

--- HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
+++ HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
@@ -11,16 +11,38 @@ Work on your solutions here.
 
 Link: https://docs.google.com/document/d/1XioaEqk6VqUPA-ccQhkqP3eAoDthxYyOM9vSPB7fDkg/edit#heading=h.uopysoy45zmw
 
-1) 
+1) Input: sudokuBoard:[[Int?]]. (Each location on the board will be either be an Int from 1-9 or nil(empty cell))
+row: Int
+col: Int
+
+//check if duplicate exists in row
+//check if duplicate exists in column
+//check if duplicate exists in box (3X3)
+
+func getValidNumbers(sudokuBoard:[[Int?]], row:Int, col:Int) -> [Int] {
+return [Int]()
+
+for (i = 1; i < 10; i++){
+    for (j = 0; j < 10; j++)
+
+if duplicates exist;
+    
+    preclude those numbers
+
+return all numbers not precluded
+
+2)
 
 
 
-2) 
+3) check if A is greater then B
 
+    check if B is greater than C
 
+    check if C is greater than D
 
-3)
+    repeat for number of elements
 
-
+    //non-optimized
 
 */
