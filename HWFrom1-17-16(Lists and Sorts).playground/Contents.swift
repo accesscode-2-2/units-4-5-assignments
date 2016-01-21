8e2847b74eb68e332f1af18ca47d49c38c293e63
Jason Wang <qwang216@gmail.com>
2016-01-21 18:38:07 -0500

    refactored sudoku code and motified question three's answer

--- HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
+++ HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
@@ -16,6 +16,62 @@ Link: https://docs.google.com/document/d/1XioaEqk6VqUPA-ccQhkqP3eAoDthxYyOM9vSPB
 
 // 1) Given a partially filled in Sudoku board and a set of coordinates in that board pointing to an empty square, write a function that returns a list containing all numbers that the empty square could be.
 
+
+func getAllColArrayIndex1(paramCol: Int, board: [[Int]]) -> [String: [Int]] {
+    // grab all the index Array that are associated with col
+    let col = paramCol-1
+    var xArray = [Int]()
+    let numberOfColBox = Int(sqrt(Double(board[0].count)))
+    var xNumber = (col)/numberOfColBox
+    for _ in 0..<numberOfColBox-1 {
+        xArray.append(xNumber)
+        xNumber += numberOfColBox
+        
+    }
+    
+    var xArrayAtIndex = [Int]()
+    var n = Int()
+    if (col % numberOfColBox == 0) {
+        n = 0
+    } else if (col % numberOfColBox == 1) {
+        n = 1
+    } else {
+        n = 2
+    }
+    for _ in 0..<numberOfColBox - 1 {
+        xArrayAtIndex.append(n)
+        n += numberOfColBox
+    }
+    return ["sudokuSmallBox": xArray, "indexNumber":xArrayAtIndex]
+}
+
+
+func getAllRowArrayIndex1(paramRow: Int, board: [[Int]]) -> [String: [Int]] {
+    // grab all the index Array that are associated with row
+    let row = paramRow - 1
+    var yArray = [Int]()
+    var yNumber = (row)/3
+    yArray.append(yNumber)
+    yNumber += 1
+    yArray.append(yNumber)
+    yNumber += 1
+    yArray.append(yNumber)
+    
+    // grall all the index number that are row Array
+    var yArrayAtIndex = [Int]()
+    if(row % 3 == 0){
+        yArrayAtIndex.appendContentsOf([0,1,2])
+    } else if (row % 3 == 1) {
+        yArrayAtIndex.appendContentsOf([3,4,5])
+    } else {
+        yArrayAtIndex.appendContentsOf([6,7,8])
+    }
+    
+    return ["sudokuSmallBox":yArray, "indexNumber":yArrayAtIndex]
+}
+
+
+
 func getAllColArrayIndex(paramCol: Int) -> [String: [Int]] {
     // grab all the index Array that are associated with col
     let col = paramCol-1
@@ -63,7 +119,8 @@ func getAllRowArrayIndex(paramRow: Int) -> [String: [Int]] {
 }
 
 func getValidNumbers(sudokuBoard: [[Int]], row: Int, col: Int) -> Set<Int>{
-    let colDictionary = getAllColArrayIndex(col)
+    let colDictionary = getAllColArrayIndex1(col, board: sudokuBoard)
+//    let colDictionary = getAllColArrayIndex(col)
     let rowDictionary = getAllRowArrayIndex(row)
     
     var colValue = [Int]()
@@ -86,7 +143,7 @@ func getValidNumbers(sudokuBoard: [[Int]], row: Int, col: Int) -> Set<Int>{
     return completeNumber.subtract(combinedRowColValue)
 }
 
-let sudokuBoard =   [[5,0,8,9,0,0,0,0,0],
+let sampleInput =   [[5,0,8,9,0,0,0,0,0],
                     [0,7,3,6,0,0,9,0,8],
                     [1,9,0,4,0,8,0,3,5],
                     [0,7,0,0,0,2,0,1,0],
@@ -95,7 +152,7 @@ let sudokuBoard =   [[5,0,8,9,0,0,0,0,0],
                     [1,9,0,2,0,3,0,8,7],
                     [3,0,6,0,0,7,1,9,0],
                     [0,0,0,0,0,9,3,0,4]]
-getValidNumbers(sudokuBoard, row: 4, col: 4)
+getValidNumbers(sampleInput, row: 4, col: 4)
 
 
 
@@ -135,5 +192,5 @@ rotateMatrixNinetyDegree(matrix)
 // 3) Design an optimal algorithm for sorting four elements A, B, C, and D. By optimal, I mean one that sorts using the minimum number of comparisons. Hint: you may want to start by putting the first two items in order and the last two items in order... that takes two comparisons. How many more comparisons do you need to find the minimum element? The maximum? Once you’ve found the min and max, what if any additional comparisons are needed?
 
 
-// Once the the first two and the last two are in order then compare the 3rd element with the 2nd element. If the 3rd element is greater than 2nd element then four elements are in order, if not then switch 3rd and 2nd element. If additional comparision are needed then I would compare the first and the last element.
+// Once the the first two and the last two are in order then compare the C element with the B element. If C element is greater than B element then all four elements are in order. if not then compare C with A element, if C < A then insert C before A otherwise after A.
 
