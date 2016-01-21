09c51d1635a85c4492eccb5992133d33fb2ce041
jamaal51 <jsedayao@gmail.com>
2016-01-21 18:23:54 -0500

    update HW

--- HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
+++ HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
@@ -13,29 +13,43 @@ var str = "Hello, playground"
 
 //1)
 
-func getValidNumbers(sudokuBoard:[[Int?]], row:Int, col:Int)->[Int]{
+func getValidNumbers(sudokuBoard:[[Int]], row:Int, col:Int)->[Int]{
     var arrayNums: [Int] = []
     var value: Int
 //    var x = row
 //    var y = col
     
     for row in 0...8{
+        if (row != 0){
         var x = row
         value = [x][col]
         x++
         arrayNums.append(value)
+        }
     }
     for col in 0...8{
+        if (col != 0){
         var y = col
         value = [row][y]
         y++
         arrayNums.append(value)
+        }
     }
     return arrayNums
 }
 
+let sudokuBoard: [[Int]] = [[5,0,8,0,7,3,1,9,0],
+                            [9,0,0,6,0,0,4,0,8],
+                            [0,0,0,9,0,8,0,3,5],
+                            [0,7,0,0,0,0,0,6,0],
+                            [0,0,2,0,0,0,9,0,0],
+                            [0,1,0,0,0,0,0,8,0],
+                            [1,9,0,3,0,6,0,0,0],
+                            [2,0,3,0,0,7,0,0,9],
+                            [0,8,7,1,9,0,3,0,4]
+                             ]
 
-
+getValidNumbers(sudokuBoard, row: 4, col: 4)
 //2)
 
 
