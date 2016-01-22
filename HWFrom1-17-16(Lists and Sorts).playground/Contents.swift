27f3c3b4fbd2462ff38065ca1a41987e819c9d96
Umar <umahmud@outlook.com>
2016-01-21 23:16:36 -0500

    update

--- HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
+++ HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
@@ -31,9 +31,116 @@ if duplicates exist;
 
 return all numbers not precluded
 
+func getValidNumbers(sudokuBoard:[[Int?]], row:Int, col:Int) -> [Int] {
+return [Int]()
+
+var valid: set <int> = {1,2,3,4,5,6,7,8,9}
+for c in 0 ..< SudokuBoard[row].count {
+
+    if c == col {
+continue}
+
+
+}
+if let value = SudokuBoard[row][c]
+{
+
+valid.remove(value)
+
+}
+
+let sqRow = row/3
+}
+
+//new function
+func getValidNumbers(sudokuBoard:[[Int?]], row:Int, col:Int) -> [Int] {
+var valid: Set<Int> = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for c in 0..<sudokuBoard[row].count {
+if c == col {
+continue
+}
+if let value = sudokuBoard[row][c] {
+valid.remove(value)
+}
+}
+
+for r in 0..<sudokuBoard.count {
+if r == row {
+continue
+}
+if let value = sudokuBoard[r][col] {
+valid.remove(value)
+}
+}
+
+let sqRow = row / 3
+let sqCol = col / 3
+
+for r in sqRow..<(sqRow + 3) {
+for c in sqCol..<(sqCol + 3) {
+if r == row && c == col {
+continue
+}
+if let value = sudokuBoard[r][c] {
+valid.remove(value)
+}
+}
+}
+
+// Turn the set into an array
+return Array<Int>(valid)
+}
+
+let sampleInput: [[Int?]] =
+[[5,0,8,9,0,0,0,0,0],
+[0,7,3,6,0,0,9,0,8],
+[1,9,0,4,0,8,0,3,5],
+[0,7,0,0,0,2,0,1,0],
+[0,0,0,0,0,0,0,0,0],
+[0,6,0,9,0,0,0,8,0],
+[1,9,0,2,0,3,0,8,7],
+[3,0,6,0,0,7,1,9,0],
+[0,0,0,0,0,9,3,0,4]]
+getValidNumbers(sampleInput, row: 0, col: 1)
+
 2)
 
+//start off by empty matrix
+func roatate(matrix: [[Int]]) -> [[Int]] {
+    var result: [[Int]] = []
 
+func rotate(matrix: [[Int]]) -> [[Int]] {
+let n = matrix.count
+
+var result: [[Int]] = []
+
+for _ in 0..<n {
+var row: [Int] = []
+for _ in 0..<n {
+row.append(0)
+}
+result.append(row)
+}
+
+for (r, row) in matrix.enumerate() {
+for (c, val) in row.enumerate() {
+result[c][n - r - 1] = val
+}
+}
+
+return result
+}
+
+let rotateInput = [[1,2,3,4],
+[5,6,7,8],
+[9,0,1,2],
+[3,4,5,6]]
+
+rotate(rotateInput)
+
+
+}
 
 3) check if A is greater then B
 
@@ -44,5 +151,10 @@ return all numbers not precluded
     repeat for number of elements
 
     //non-optimized
+
+    //optimized - split array in half, then merge
+
+
+
 
 */
