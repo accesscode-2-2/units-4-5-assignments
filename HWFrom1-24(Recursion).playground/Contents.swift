6afb29aefa3ebf18ff070098385738392d368a6a
charles <ckangkitchen@gmail.com>
2016-01-28 16:37:02 -0500

    added command line for question 3

--- HWFrom1-24(Recursion).playground/Contents.swift
+++ HWFrom1-24(Recursion).playground/Contents.swift
@@ -10,16 +10,33 @@ Homework link: https://docs.google.com/document/d/1INvOynuggw69yLRNg3y-TPwBiYb3l
 
 
 //Question 1
+// http://stackoverflow.com/questions/28161197/swift-vs-objective-c-fibonacci-sequence-speed-comparison
 
-
-
-
-
+func fib(num: Int) -> Int{
+    if(num == 0){
+        return 0;
+    }
+    if(num == 1){
+        return 1;
+    }
+    return fib(num - 1) + fib(num - 2);
+}
 
 //Question 2
 
 
-
-
+// not sure if this is correct, i don't think it is
+var stepNum = 0
+func newStepUp() -> Bool {
+    let success = Int(arc4random_uniform(4)) > 0
+    if (success) {
+        stepNum++
+        print("Yay \(stepNum)")
+    } else {
+        stepNum--
+        print("Noo \(stepNum)")
+    }
+    return success
+}
 
 //Question 3
