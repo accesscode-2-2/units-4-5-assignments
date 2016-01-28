a4541682b59b8e999ff0a4c5abfe6c0d4ff18f01
Xiulan <xiulanshi2013@gmail.com>
2016-01-28 17:10:57 -0500

    Added

--- HWFrom1-24(Recursion).playground/Contents.swift
+++ HWFrom1-24(Recursion).playground/Contents.swift
@@ -3,6 +3,65 @@
 
 Homework link: https://docs.google.com/document/d/1INvOynuggw69yLRNg3y-TPwBiYb3lQZQiFUOxZKBwsY/edit#heading=h.za36ai6n5fth
 
+Write an iterative (not recursive) fibonacci function that calculates the nth fibonacci number. How does its performance compare with the non-memoized recursive one (see Appendix A below), based on the number of iterations that Swift says it takes in the right margin?
+
+
+The engineers have been hard at work on the clumsy robot project, and have released a new API with a new tryStep method (see Appendix B). Now it returns an Int, which is -1 if the robot fell down a step, 0 if the robot stayed on the same step, or 1 if the robot went to the next step. Write a new stepUp method using this new tryStep method that works the same as before.
+
+
+Using the code in Appendix C as a starting point, create a Swift command line project to find files on your computer by name. Your solution should use recursion. Your method should return “NOT FOUND” if it couldn’t find the file, otherwise it should return the full path to that file.
+
+You can’t use a playground for this because they don’t have filesystem access for some reason. Instead, in XCode, go to File > New > Project, select “Application” under “OS X” in the sidebar, then select “Command Line Tool”; on the next screen, choose “Swift” as the language.
+
+
+Appendix A: Recursive Fibonacci from class (non-memoized)
+func fib(n: Int) -> Int {
+print("X")
+if (n == 0 || n == 1) {
+return 1
+}
+return fib(n - 1) + fib(n - 2)
+}
+Appendix B: New and improved clumsy robot
+import Foundation
+
+var stepNum = 0
+func tryStep() -> Int {
+let stepCount = Int(arc4random_uniform(3)) - 1
+stepNum += stepCount;
+switch(stepCount) {
+case -1: print("Ouch \(stepNum)")
+case 1: print("Yay  \(stepNum)")
+default: print("Beep \(stepNum)")
+}
+return stepCount
+}
+
+Appendix C: File searching
+import Foundation
+
+func findFile(name: String, atPath: String) -> String {
+let fileManager = NSFileManager.defaultManager()
+let contents =
+try! fileManager.contentsOfDirectoryAtPath(atPath)
+for fileOrDir in contents {
+var isDir = ObjCBool(false);
+let fullPath = atPath + "/" + fileOrDir
+let exists = fileManager.fileExistsAtPath(fullPath, isDirectory: &isDir)
+if exists && Bool(isDir) {
+// YOUR CODE HERE
+print("DIR: " + fileOrDir)
+} else if exists {
+// YOUR CODE HERE
+print("FILE: " + fileOrDir)
+} else {
+print("NEITHER: " + fileOrDir)
+}
+}
+return "NOT FOUND"
+}
+
+print(findFile("awesome-idea.txt", atPath: "/Users/calebegg/Documents"))
 
 
 */
--- HWfrom1-23-16(Recursion).playground/Contents.swift
+++ HWfrom1-23-16(Recursion).playground/Contents.swift
@@ -7,6 +7,30 @@ var str = "Hello, playground"
 
 //https://docs.google.com/document/d/1KfnTOtPnBrYPFhBRAQPZBXor_mKDQvuJp4zwZbtHkRs/edit#heading=h.16sfqfmanxte
 
+/*
+Write a recursive method to print the binary representation of a decimal number to STDOUT. https://www.khanacademy.org/math/pre-algebra/applying-math-reasoning-topic/alternate-number-bases/v/decimal-to-binary - explanation of how to convert decimal to binary
+
+
+func printBinaryRepresentation(number:Int) {
+
+}
+
+Sample Input: 		5
+Sample Output: 	101
+
+2.	Swap all adjacent elements in an array. All arrays will have an even size. Do not use loops.
+
+Sample input: 		[1,2,4,7,3,8,5,6]
+Sample output:	[2,1,7,4,8,3,6,5]
+
+3. Implement a binary search for a sorted array. Given an Int and an [Int]. Return true if the element exists in the array and false if not.
+
+Sample input: 		[1,2,7,23,23,167,9465]     167
+Sample output:	true
+
+
+*/
+
 
 //1
 
