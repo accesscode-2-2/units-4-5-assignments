1c54db4adbc735bf983a9e89ab437037da1a8df1
Diana <diana_elezaj@hotmail.com>
2016-01-30 00:01:23 -0500

    Merge sort homework - Done

--- HWFrom1-28-16(Merge Sort).playground/Contents.swift
+++ HWFrom1-28-16(Merge Sort).playground/Contents.swift
@@ -4,3 +4,34 @@
 
 
 //Insert code here:
+
+func insertionSort(var array:[Int], index:Int) -> [Int]{
+    
+    if index == array.count{
+        return array
+    }
+    else{
+        
+        var tempArray = array[index],
+        i = 0,
+        j = 0,
+        maxIter = index - 1
+        
+        while (i <= maxIter  &&  array[i] < tempArray) {
+            i++
+        }
+        while j<=maxIter{
+            array[maxIter-j+1] = array[maxIter-j]
+            j++
+        }
+        
+        array[i] = tempArray;
+        
+        return insertionSort(array, index: index+1)
+    }
+    
+}
+
+//Test
+insertionSort([44, 7, 2, 25, 0], index: 1)
+
