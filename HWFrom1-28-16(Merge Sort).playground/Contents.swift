ae28ae19997a74f2f72042682ade90055a7ab62c
Xiulan <xiulanshi2013@gmail.com>
2016-01-31 16:17:36 -0500

    Updated

--- HWFrom1-28-16(Merge Sort).playground/Contents.swift
+++ HWFrom1-28-16(Merge Sort).playground/Contents.swift
@@ -4,3 +4,30 @@
 
 
 //Insert code here:
+/*
+Use recursion to implement at least one of the following sorting algorithms in Swift:
+
+// Insertion sort
+func insertionSort(inout values: [Int]) {
+}
+
+// Selection sort
+func selectionSort(inout values: [Int]) {
+}
+
+Requirements:
+You may not use loops
+Your implementations should be in-place (try not to create additional arrays)
+You should implement and use additional helper functions
+
+Tips:
+Add additional parameters to your helper functions to pass information between recursive calls
+You can use the swap function to exchange two values in a mutable (var) array:
+var array = [1, 2]
+swap(&array[0], &array[1])
+array // [2, 1]
+
+Challenge:
+Modify your implementation to work for any array of Comparable elements
+
+*/
--- HWFrom1-30-16(QuickSort+Lists+Queues).playground/Contents.swift
+++ HWFrom1-30-16(QuickSort+Lists+Queues).playground/Contents.swift
@@ -3,14 +3,28 @@
 //Answer the questions in the homework below
 //https://docs.google.com/document/d/1KlK3PmRMybmHS5db_AP11uHXIcbIeM5vHuuAZOV3xME/edit#
 
-//1)
+//1)Without looking at the Big O Cheatsheet, write down the average time and space complexity for bubble sort, insertion sort, selection sort, mergesort, and quicksort.
 
-//2)
+//2)What is the advantage of partitioning quicksort in place?
 
-//3)
+//3)Without looking, implement quicksort.
 
-//4)
 
-//5)
+//4)Write a function to generate an array of random numbers bounded between 1..<10,000 of size 10,000.
 
-//6)
+//Int(arc4random_uniform(UInt32(10000)))
+
+//Compare the time it takes to run mergesort, quicksort, and quicksort with the median.
+//https://gist.github.com/gummibeatz/8ff29bcec54d7e3ef683
+
+
+//5)Describe the algorithmic difference between mergesort and quicksort. Where does the sorting happen? As the recursive calls are being pushed onto the stack or as they are being popped off?
+
+//6)Given an array of strings containing “[“,”]”,”{“,”}”,”(“,”)”. Output whether or not the parentheses are balanced.
+//Good examples: () [] () ([]()[])
+//Bad examples: ( ( ] ([)]
+//
+//func isBalanced(paren: [String]) -> Bool {
+//
+//}
+
