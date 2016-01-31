d35bc35c6efa95a4bf84682ed085669033e4cc7c
elberdev <elberdev@gmail.com>
2016-01-31 07:53:05 -0500

    added 1-30 hw

--- HWFrom1-30-16(QuickSort+Lists+Queues).playground/Contents.swift
+++ HWFrom1-30-16(QuickSort+Lists+Queues).playground/Contents.swift
@@ -1,5 +1,20 @@
 //: Playground - noun: a place where people can play
 
+// FOR REFERENCE, insertion sort
+func insertionSort(inout numberList:[Int]){
+    var x, y, key : Int
+    for (x = 0; x < numberList.count; x++) {
+        
+        key = numberList[x]
+        for (y = x; y > -1; y--) {
+            if (key < numberList[y]) {
+                numberList.removeAtIndex(y + 1)
+                numberList.insert(key, atIndex: y)
+            }
+        }
+    }
+}
+
 //Answer the questions in the homework below
 //https://docs.google.com/document/d/1KlK3PmRMybmHS5db_AP11uHXIcbIeM5vHuuAZOV3xME/edit#
 
