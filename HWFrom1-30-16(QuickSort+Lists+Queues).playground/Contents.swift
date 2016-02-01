b9a19c15468cb8e17814ccac0b83e8f802015710
Xiulan <xiulanshi2013@gmail.com>
2016-02-01 00:23:00 -0500

    Finished 01-30 HW

--- HWFrom1-30-16(QuickSort+Lists+Queues).playground/Contents.swift
+++ HWFrom1-30-16(QuickSort+Lists+Queues).playground/Contents.swift
@@ -139,8 +139,31 @@ In contrast, quickSort is exactly the opposite, namely top down recursion in the
 //Bad examples: ( ( ] ([)]
 //
 func isBalanced(paren: [String]) -> Bool {
-    
-
-    return true
+    var stack = [String]()
+    for (var i = 0; i < paren.count; i++) {
+        let check = paren[i]
+        if (check == "(" || check == "[" || check == "{") {
+            stack.append(check);
+        } else if (stack.count == 0) {
+            return false
+        } else if (check == ")" && stack.removeLast() != "(") {
+            return false
+        } else if (check == "]" && stack.removeLast() != "[") {
+            return false
+        } else if (check == "}" && stack.removeLast() != "{") {
+            return false
+        }
+    }
+    return true;
 }
 
+var good = ["(", ")", "[", "]", "(", ")", "(", "[", "]", "(", ")", "[", "]", ")"]
+var bad = ["(", "(", "]", "(", "[", ")", "]"]
+var test = [")", ")"]
+var test2 = ["{","[", "]","}", "{", "}"]
+
+isBalanced(good)
+isBalanced(bad)
+isBalanced(test)
+isBalanced(test2)
+isBalanced([])
