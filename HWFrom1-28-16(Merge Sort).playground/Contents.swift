45bbfc0b9a8584ac23e3dd239fbeedd1c27ca754
jamaal51 <jsedayao@gmail.com>
2016-02-04 15:25:18 -0500

    update hw

--- HWFrom1-28-16(Merge Sort).playground/Contents.swift
+++ HWFrom1-28-16(Merge Sort).playground/Contents.swift
@@ -27,43 +27,45 @@ selectionSort(&array(arrayNums))
 
 
 
-func printAllElements(values: [Int]) {  //takes the array and just prints each element
-    for value in values {
-        print(value)
-    }
-}
-
-func printAllElementsRecursive(values: [Int]) {  //
-    printElementsHelper(values, index: 0)
-}
-
-func printElementsHelper(values: [Int], index: Int) {
-    if index < values.count {
-        print(values[index])
-        printElementsHelper(values, index: index + 1)
-    }
-}
-
-func setValue(inout array: [Int], value: Int, atIndex index: Int) {
-    array[index] = value
-}
-
-var values = [10, 20, 30]
-printAllElements(values)
-
-setValue(&values, value: 100, atIndex: 1)
-values
-
-func reverse(inout array: [Int]) {
-    for i in 0..<array.count / 2 {
-        swap(&array[i], &array[array.count - i - 1])
-    }
-}
-reverse(&values)
-values
 
 
-printAllElements(arrayNums)
+//func printAllElements(values: [Int]) {  //takes the array and just prints each element
+//    for value in values {
+//        print(value)
+//    }
+//}
+//
+//func printAllElementsRecursive(values: [Int]) {  //
+//    printElementsHelper(values, index: 0)
+//}
+//
+//func printElementsHelper(values: [Int], index: Int) {
+//    if index < values.count {
+//        print(values[index])
+//        printElementsHelper(values, index: index + 1)
+//    }
+//}
+//
+//func setValue(inout array: [Int], value: Int, atIndex index: Int) {
+//    array[index] = value
+//}
+//
+//var values = [10, 20, 30]
+//printAllElements(values)
+//
+//setValue(&values, value: 100, atIndex: 1)
+//values
+//
+//func reverse(inout array: [Int]) {
+//    for i in 0..<array.count / 2 {
+//        swap(&array[i], &array[array.count - i - 1])
+//    }
+//}
+//reverse(&values)
+//values
+//
+//
+//printAllElements(arrayNums)
 
 
 
