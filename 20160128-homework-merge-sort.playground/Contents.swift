6961767a28360d25cb1e4d52a3d969d140ab2f5c
shenayoshida <sheyoshida@gmail.com>
2016-02-05 13:53:50 -0500

    class notes added

--- 20160128-homework-merge-sort.playground/Contents.swift
+++ 20160128-homework-merge-sort.playground/Contents.swift
@@ -101,3 +101,59 @@ reverse(&values)
 values
 
 
+// 2) 
+
+let blacklist: Set<String> = ["crapple", "fandroid", "m$"]
+
+func moderate(message: String) -> Bool {
+    
+    let words = (message as NSString).componentsSeparatedByString(" ")
+    for word in words {
+        if blacklist.contains(word.lowercaseString) {
+            return false
+        }
+    }
+    return true
+}
+moderate("I would never use a crApple product!")
+moderate("something else")
+
+// 3) 
+
+protocol PhoneBookProtocol {
+    mutating func addPerson(name: String, phoneNumber: String)
+    mutating func removePerson(name: String)
+    mutating func importFrom(oldPhonebook: [(String, String)])
+    func findPerson(name: String) -> String? // Return phone #
+}
+
+class PhoneBook: PhoneBookProtocol {
+    var storage: [String : String] = [:] // @{}
+    //var storage = Dictionary<String, String>()
+    func addPerson(name: String, phoneNumber: String) {
+        storage[name] = phoneNumber
+    }
+    
+    func removePerson(name: String) {
+        storage.removeValueForKey(name)
+    }
+    
+    func findPerson(name: String) -> String? {
+        return storage[name]
+    }
+    
+    func importFrom(oldPhonebook: [(String, String)]) {
+        for entry in oldPhonebook {
+            addPerson(entry.0, phoneNumber: entry.1)
+        }
+    }
+}
+
+
+let oldData = [("Caleb", "501-555-1234"), ("Mike", "212-555-4321"), ("Jenny", "345-867-5309")]
+
+let phoneBook = PhoneBook()
+phoneBook.importFrom(oldData)
+
+phoneBook.findPerson("Jenny")
+
