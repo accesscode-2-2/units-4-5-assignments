b7c016c6095ad63df5676d68ccadb4a88cdf17ab
cdolm92 <cdolm92@gmail.com>
2016-02-12 20:30:56 -0500

    working on big o

--- HWfrom1-10-016(BigO).playground/Contents.swift
+++ HWfrom1-10-016(BigO).playground/Contents.swift
@@ -11,11 +11,20 @@ Use the link here to get the questions.  Then code your solutions below.  If it 
 https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/edit#heading=h.za36ai6n5fth
 
 
-1)
+1)  a. O(m * n)
+    b. O(n^2)
+    c. O(n^4)
 
-2)
 
-3)
+
+2)  a. O(n^4)
+    b. O(n)
+    c. O(n^2)
+    d. O(n)
+
+3)  a. graph
+    b. graph
+    c. hash table
 
 4)
 
@@ -27,4 +36,39 @@ https://docs.google.com/document/d/1aF1imJUVahCSJAuN1OEm5lQXwpSFaAmVmAETKMM6PLQ/
 
 
 */
+
+
+
+func factorialCalc(num: Int) -> Int {
+   
+    let range = 1...num
+    var multiplier = 0
+    
+    
+    
+    
+
+    return multiplier
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
