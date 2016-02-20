4f8dbf5ea8f190d9b2ee9a6735f6ccede1155179
Natalia Estrella <natalia.c.estrella@gmail.com>
2016-02-20 00:57:46 -0500

    From Gist https://gist.github.com/NataliaEstrella/97865c26f2bf66a0e715

---
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -0,0 +1,74 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+
+Question 1: https://www.hackerrank.com/challenges/minimum-draws
+
+Copy and paste your code:
+
+int main() {
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+int testCases;
+cin >> testCases;
+
+for (int i = 0; i < testCases; i++) {
+int pairs;
+cin >> pairs;
+pairs = pairs + 1;
+cout << pairs << endl;
+}
+
+return 0;
+}
+
+What is the big O runtime of your code?: 0(1)
+
+Question 2: https://www.hackerrank.com/challenges/handshake
+
+Copy and paste your code:
+
+= n choose r
+int n = numberOFPeopleInRoom
+int r = numberOfPeopleShakingHands
+
+= n! / (r! * (n-r)!)
+Case 1
+
+= 1!/(2! *(1-2)!)
+= 0
+
+Case 2
+= 2! / (2! * (2-2)!)
+
+What is the big O runtime of your code?:
+
+Question 3: https://www.hackerrank.com/challenges/connecting-towns
+
+Copy and paste your code:
+
+int main() {
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+int testCases;
+cin >> testCases;
+
+for (int i = 0; i < testCases; i++) {
+int squadCount;
+cin >> squadCount;
+int handshakes = squadCount*(squadCount - 1)/2;
+//n choose r = n! / (r! * (n-r)!)
+//n choose 2 = n! / (2! * (n-2)!)
+// = n! / (2*(n-2)!)
+//
+cout << handshakes << endl;
+}
+
+return 0;
+}
+
+
+What is the big O runtime of your code?: ?
+*/
