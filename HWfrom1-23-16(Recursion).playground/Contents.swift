c36d6fa43c2958105f8f3a0ce369a73fc8ffa652
Natalia Estrella <natalia.c.estrella@gmail.com>
2016-02-20 01:01:37 -0500

    Almost

---
+++ HWfrom1-23-16(Recursion).playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//https://docs.google.com/document/d/1KfnTOtPnBrYPFhBRAQPZBXor_mKDQvuJp4zwZbtHkRs/edit#heading=h.16sfqfmanxte
+
+
+
+func printBinaryRepresentation(number:Int) {
+    
+}
+
+//1
+
+
+
+
+//2
+
+
+
+
+//3
