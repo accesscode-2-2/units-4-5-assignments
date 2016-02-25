d7b9da96db82ae1b871339804e205f9705623bb2
cdolm92 <cdolm92@gmail.com>
2016-02-25 18:39:44 -0500

    insertion sorts

--- HWFrom1-28-16(Merge Sort).playground/Contents.swift
+++ HWFrom1-28-16(Merge Sort).playground/Contents.swift
@@ -4,3 +4,29 @@
 
 
 //Insert code here:
+
+func insertionSort(numberList: [Int]) -> [Int]
+{
+     
+    //mutated copy
+    var output = numberList
+     
+    for primaryIndex in 0..<output.count {
+         
+        let key = output[primaryIndex]
+         
+        for var secondaryIndex = primaryIndex; secondaryIndex > -1; secondaryIndex-- {
+             
+            //move into correct position
+            if key < output[secondaryIndex] {
+                output.removeAtIndex(secondaryIndex + 1)
+                output.insert(key, atIndex: secondaryIndex)
+            }
+        }
+    }
+    
+    return output
+    
+}
+
+insertionSort([4,5,2,88,7,11])
